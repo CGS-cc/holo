c29cb739603ae1360d920677b18d24485b8cd7a4
Chris Spoonts <cspoonts@protonmail.com>
2023-01-31 21:13:24 -0600

    safe

--- src/App.svelte
+++ src/App.svelte
@@ -20,61 +20,13 @@
 
 <main>
 	<CardList>
-    <span>
 				<Card
 					name="Bulbasaur"
 					img="https://dl.dropboxusercontent.com/s/90ef0fqjxahoed3/dauntless.png?raw=1"
 					number="TG241"
 					supertype="regular"
-					subtypes={["Basic"]}
-					rarity="Rare Holo"
-				/></span>
-    <span>
-        <Card
-					name="Articuno"
-					img="https://dl.dropboxusercontent.com/s/b3bj4mv45u3zbme/dwarfling.png?raw=1"
-					number="TG241"
-					supertype="regular"
-					subtypes={["Basic"]}
+					subtypes="basic"
 					rarity="Rare Holo"
 				/>
-    </span>
-    <span>
-    <Card
-					name="Pikachu"
-					img="https://dl.dropboxusercontent.com/s/28h0h9pzcbo1qck/equanimous.png?raw=1"
-					number="TG241"
-					supertype="regular"
-					subtypes={["Basic"]}
-					rarity="Rare Holo"
-				/></span>
-        <span><Card
-					name="Radiant Venusaur"
-					img="https://dl.dropboxusercontent.com/s/8vs93s1rpa3a2a2/colossus.png?raw=1"
-					number="TG241"
-					supertype="regular"
-					subtypes={["Basic"]}
-					rarity="Rare Holo"
-				/></span>
-          <span>
-           <Card
-					name="Pikachu"
-					img="https://dl.dropboxusercontent.com/s/6bm5wseez3nf9ee/peregrination.png?raw=1"
-					number="TG241"
-					supertype="regular"
-					subtypes={["Basic"]}
-					rarity="Rare Holo"
-				/></span>
   </CardList>
 </main>
-
-<div class="back-to-top">
-  <a href="#⚓-top">Back to Top</a>
-</div>
-
-<style>
-  .back-to-top a {
-    color: inherit;
-    text-decoration: none;
-  }
-</style>
