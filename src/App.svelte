ebcfb9a64708431445690c3803ff90245ac1efeb
Chris Spoonts <cspoonts@protonmail.com>
2023-02-05 13:45:38 -0600

    safe

--- src/App.svelte
+++ src/App.svelte
@@ -7,4 +7,21 @@
 					img="https://dl.dropboxusercontent.com/s/90ef0fqjxahoed3/dauntless.png?raw=1"
 					rarity="Rare Holo"
 				/>
+        <Card
+					img="https://dl.dropboxusercontent.com/s/b3bj4mv45u3zbme/dwarfling.png?raw=1"
+					rarity="Rare Holo"
+				/>
+        <Card
+					img="https://dl.dropboxusercontent.com/s/28h0h9pzcbo1qck/equanimous.png?raw=1"
+					rarity="Rare Holo"
+				/>
+        <Card
+					img="https://dl.dropboxusercontent.com/s/8vs93s1rpa3a2a2/colossus.png?raw=1"
+					rarity="Rare Holo"
+				/>
+        <Card
+					img="https://dl.dropboxusercontent.com/s/6bm5wseez3nf9ee/peregrination.png?raw=1"
+					rarity="Rare Holo"
+				/>
+
 </main>
